97383e6ca3a74f8bab7f9b7ab719ef6c784b4991
Mohy Elden <Mohieelden50@gmail.com>
2024-09-27 11:22:21 +0300

    split css style from ejs

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
 
 app.use(logger('dev'));
 app.use(express.json());
